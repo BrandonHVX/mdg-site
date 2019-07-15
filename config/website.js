f6475ffdb212f013ab83af697078cd2e43a4facf
Brandon <brandongines@gmail.com>
2019-07-14 21:44:03 -0400

    icons

--- config/website.js
+++ config/website.js
@@ -1,18 +1,18 @@
 module.exports = {
 	pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-	siteTitle: 'Emma', // Navigation and Site Title
-	siteTitleAlt: 'Emma - Gatsby Starter Portfolio', // Alternative Site title for SEO
+	siteTitle: 'Milq Da Game Entertainment', // Navigation and Site Title
+	siteTitleAlt: 'Milq Da Game', // Alternative Site title for SEO
 	siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
-	siteTitleShort: 'Emma', // short_name for manifest
-	siteUrl: 'https://emma.lekoarts.de', // Domain of your site. No trailing slash!
+	siteTitleShort: 'Milq Da Game', // short_name for manifest
+	siteUrl: 'https://www.mdgent.com', // Domain of your site. No trailing slash!
 	siteLanguage: 'en', // Language Tag on <html> element
-	siteLogo: '/mdgblack.png', // Used for SEO and manifest
+	siteLogo: './components/mdgblack.png', // Used for SEO and manifest
 	siteDescription: 'Minimalistic bright portfolio with full-width grid and large images',
-	author: 'LekoArts', // Author for schema.org JSONLD
+	author: 'MDG', // Author for schema.org JSONLD
 
 	// siteFBAppID: '123456789', // Facebook App ID - Optional
-	userTwitter: '@emma', // Twitter Username
-	ogSiteName: 'emma', // Facebook Site Name
+	userTwitter: '@milqdagame', // Twitter Username
+	ogSiteName: 'MDG', // Facebook Site Name
 	ogLanguage: 'en_US',
 	googleAnalyticsID: 'UA-47519312-3',
 
